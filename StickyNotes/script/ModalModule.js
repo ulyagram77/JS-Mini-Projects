909f4f0abf3c2444f88f744b2878384113fe0ba1
whitegolyb <kirillulianow@gmail.com>
2023-10-06 17:23:08 +0300

    feat: added settings button and modal transitions

--- StickyNotes/script/ModalModule.js
+++ StickyNotes/script/ModalModule.js
@@ -1,16 +1,31 @@
 const modalWindow = document.querySelector('.modal'),
-      closeModalWindow = document.querySelector('.modal__close');
+      closeModalWindow = document.querySelector('.modal__close'),
+      settingsButton = document.querySelector('.settings');
 
 document.addEventListener('DOMContentLoaded', function () {
     modalWindow.style.display = 'block';
 });
 
-modalWindow.addEventListener('click', function(event) {
+modalWindow.addEventListener('click', function (event) {
     if (event.target.classList.contains('modal')) {
-        modalWindow.style.display = 'none';
+        modal.style.opacity = '0';
+        setTimeout(function () {
+            modalWindow.style.display = 'none';
+        }, 300);
     }
 });
 
-closeModalWindow.addEventListener('click', function(event) {
-    modalWindow.style.display = 'none';
+closeModalWindow.addEventListener('click', function (event) {
+    modal.style.opacity = '0';
+    setTimeout(function () {
+        modalWindow.style.display = 'none';
+    }, 300);
+});
+
+settingsButton.addEventListener('click', function(event) {
+    modalWindow.style.display = 'block';
+    setTimeout(function () {
+        modal.style.opacity = '1';
+    }, 20);
+    
 });
